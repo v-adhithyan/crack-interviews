79493151bc846ac082829e48f504d5ba5c42bf24
Adhithyan <v.adhithyan@gmail.com>
2026-06-12 21:31:50 +0530

    add website landing page

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.core.urls")),
+    path("", include("apps.website.urls")),
 ]
