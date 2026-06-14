65d342a86f12c5c72c22fe353f936a47f45123cc
Adhithyan <v.adhithyan@gmail.com>
2026-06-14 08:37:36 +0530

    change admin title

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -10,3 +10,7 @@ urlpatterns = [
 
 handler404 = "apps.website.views.page_not_found"
 handler500 = "apps.website.views.server_error"
+
+admin.site.site_header = "HackerLeap"
+admin.site.site_title = "HackerLeap Admin"
+admin.site.index_title = "Welcome to HackerLeap Admin"
